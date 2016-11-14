d37cfb89a6ce706bc4b8a52e2eab0e9a78982924
DeathNebula <deathnebula@gmail.com>
2016-11-14 19:50:17 +0100

    Third commit

--- src/com/connected3d/test/Main.java
+++ src/com/connected3d/test/Main.java
@@ -1,5 +1,7 @@
 package com.connected3d.test;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,7 +10,24 @@ public class Main {
 	}
 	
 	public static void runcode() {
-		System.out.print("I run correctly");
+		System.out.println("I run correctly");
+		System.out.println("Because A + B = C");
+		int a = askInteger("A = ");
+		int b = askInteger("B = ");
+		int c = a + b;
+		System.out.println("C equals " + c);
+		a = a * a;
+		System.out.println("And A squared equals " + a);
+	}
+	public static int askInteger(String str) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(str);
+		while (!scan.hasNextInt()) {
+			System.out.print("Please enter an interger: ");			
+			scan.next();
+		}	
+		int input = scan.nextInt();
+		return input;
 	}
 
 }
