6a7cc8ffe9b06f0f96085052938fb766a8643bf2
DeathNebula <deathnebula@gmail.com>
2016-11-14 19:23:04 +0100

    Second commit

---
+++ src/com/connected3d/test/Main.java
@@ -0,0 +1,14 @@
+package com.connected3d.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		runcode();
+
+	}
+	
+	public static void runcode() {
+		System.out.print("I run correctly");
+	}
+
+}
